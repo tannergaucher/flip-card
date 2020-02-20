6adbfa31140e8a2ce3dafabfc1b7a280613726a4
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2020-02-20 11:42:02 -0500

    Fix edit card form nested delete btn

--- src/components/card/edit-card-form.js
+++ src/components/card/edit-card-form.js
@@ -13,65 +13,67 @@ export default function EditCardForm({ frontText, backText, cardId }) {
   const history = useHistory()
 
   return (
-    <fieldset className="fieldset" disabled={loading}>
-      <form
-        className="form"
-        onSubmit={async e => {
-          e.preventDefault()
-          setLoading(true)
+    <>
+      <fieldset className="fieldset" disabled={loading}>
+        <form
+          className="form"
+          onSubmit={async e => {
+            e.preventDefault()
+            setLoading(true)
 
-          const token = localStorage.getItem('token')
-          const res = await fetch(`/.netlify/functions/edit-card`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              frontText: newFrontText,
-              backText: newBackText,
-              cardId,
-              token,
-            }),
-          })
+            const token = localStorage.getItem('token')
+            const res = await fetch(`/.netlify/functions/edit-card`, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                frontText: newFrontText,
+                backText: newBackText,
+                cardId,
+                token,
+              }),
+            })
 
-          if (res.ok) {
-            const {
-              data: { card },
-            } = await res.json()
+            if (res.ok) {
+              const {
+                data: { card },
+              } = await res.json()
 
-            const foundIndex = data.cards.findIndex(
-              contextCard => contextCard._id === card._id
-            )
+              const foundIndex = data.cards.findIndex(
+                contextCard => contextCard._id === card._id
+              )
 
-            data.cards[foundIndex] = card
-            setMyCards(data)
-            setLoading(false)
-            history.push(`/`)
-          } else {
-            const { error } = await res.json()
-            console.log(error)
-            setLoading(false)
-          }
-        }}
-      >
-        <input
-          className="input"
-          type="text"
-          defaultValue={frontText}
-          onChange={e => setNewFrontText(e.target.value)}
-        />
-        <input
-          className="input"
-          type="text"
-          defaultValue={backText}
-          onChange={e => setNewBackText(e.target.value)}
-        />
-        <button className="btn btn-primary" type="submit" disabled={loading}>
-          Save Changes
-        </button>
-        <br />
-        <DeleteCardButton cardId={cardId} />
-      </form>
-    </fieldset>
+              data.cards[foundIndex] = card
+              setMyCards(data)
+              setLoading(false)
+              history.push(`/`)
+            } else {
+              const { error } = await res.json()
+              console.log(error)
+              setLoading(false)
+            }
+          }}
+        >
+          <input
+            className="input"
+            type="text"
+            defaultValue={frontText}
+            onChange={e => setNewFrontText(e.target.value)}
+          />
+          <input
+            className="input"
+            type="text"
+            defaultValue={backText}
+            onChange={e => setNewBackText(e.target.value)}
+          />
+          <button className="btn btn-primary" type="submit" disabled={loading}>
+            Save Changes
+          </button>
+          <br />
+        </form>
+      </fieldset>
+      <DeleteCardButton cardId={cardId} />
+    </>
   )
 }
